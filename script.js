45b312b1ed2ae7a6159571c9ecca6cd4ddfd4b42
anmolkhuwaja <anmolkhuwaja.xwave@gmail.com>
2024-09-08 09:31:01 -0700

    fetch data from API & show on cards

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const searchInput = document.getElementById('searchInput');
         searchInput.classList.toggle('show');
 });
 
-
-
 // Cards Pagination
 document.addEventListener('DOMContentLoaded', function () {
     new Swiper('.swiper', {
@@ -25,3 +23,37 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
+
+
+//fetch data from API
+let productCard = document.querySelector(".product");
+
+let displayProduct = async () => {
+  productCard.innerHTML = '';
+  let product = await fetch('https://fakestoreapi.com/products');
+  let finalProduct = await product.json();
+
+  // Use slice to get the first 8 items
+  finalProduct.slice(0, 8).forEach(element => {
+    productCard.innerHTML += `
+      <div class="card product-card h-100 border-0 shadow-none d-flex flex-column">
+        <div class="position-relative z-2 flex-grow-1">
+          <a class="d-block p-2 p-lg-3" href="shop-product-grocery.html">
+            <div class="pic">
+              <img src="${element.image}" alt="Image" class="img-fluid" style="max-height: 150px; object-fit: contain;"> <!-- Adjust image size -->
+            </div>
+          </a>
+          <div class="card-body pt-0 px-1 px-md-2 px-lg-3 pb-2 flex-grow-1 d-flex flex-column">
+            <div class="h6 mb-2">${element.price}</div>
+            <h6 class="fs-sm lh-base mb-2 text-truncate" style="max-height: 3rem; overflow: hidden; text-overflow: ellipsis;">
+              <a class="cardtext" href="shop-product-grocery.html">${element.title}</a>
+            </h6>
+          </div>
+        </div>
+        <div class="fs-xs text-body-secondary px-1 px-md-2 px-lg-3 pb-2 pb-md-3">${element.rating.count}</div>
+        <button class="btn bg-black text-white fw-medium mb-2 rounded-pill mt-auto">ADD TO CART</button>
+      </div>`;
+  });
+};
+
+displayProduct();
